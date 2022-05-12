4650ddbdb0d4da1b34b560edbfd822942ef51aac
Marco-Gagliano <brazil_94@hotmail.it>
2022-05-12 16:19:14 +0200

    aggiunta-foto-bonus

--- src/components/JumbotronPage.vue
+++ src/components/JumbotronPage.vue
@@ -2,6 +2,7 @@
 
   <div class="jumbotron">
     <h1>E questo è il Bonus Jumbotron </h1>
+    <img src="https://static.turbosquid.com/Preview/2020/09/21__17_11_05/jumbotron6.jpgEEF62DED-8B43-49C2-91FF-E06795F30AC1Large.jpg" alt="" srcset="">
   </div>
 
 </template>
@@ -20,6 +21,10 @@ export default {
     border: 2px solid #FF0000;
     text-align: center;
     padding: 10px 15px;
+  }
+
+  .jumbotron-image {
+    
   }
 
 </style>
